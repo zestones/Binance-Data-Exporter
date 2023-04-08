b97f1d6fb0d2ed0c385ca834a892162a76fd130d
Idriss <idrissbenguezzou@gmail.com>
2023-04-08 16:40:47 +0200

    fix setup version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='binance_data_exporter',
-    version='0.2.0',  # current_version
+    version='0.2.1',  # current_version
     description='A tool for exporting in JSON the historical data of a symbol from Binance',
     author='zestones',
     author_email='idrissbenguezzou@gmail.com',
